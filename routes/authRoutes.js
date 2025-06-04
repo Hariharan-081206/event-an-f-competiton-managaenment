45885197628eb5155d058283b43329850dfc935a
Hariharan-081206 <harivisu06@gmail.com>
2025-06-04 13:55:17 +0530

    authtication router

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,17 +9,14 @@ router.get('/logout', logout);
 
 export default router;*/
 
-import express from 'express';
-import { createHost } from '../controllers/hostControllers.js';
-import isHost from '../middleware/isHost.js';
-import { googleAuth,googleAuthCallback,logout } from '../controllers/authControllers.js';
+import { Router } from 'express';
+import { googleAuth, googleAuthCallback, logout } from '../controllers/authControllers.js';
+import { getCurrentUser } from '../controllers/userControllers.js';
+const router = Router();
 
-const router = express.Router();
-
-router.post('/add-host', createHost);
 router.get('/auth/google', googleAuth);
 router.get('/auth/google/callback', googleAuthCallback);
-
-router.use(isHost)
+router.get('/logout', logout);
+router.get('/current-user',getCurrentUser);
 
 export default router;
