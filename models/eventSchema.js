5b1e675360a2ba3f5e436d03112457569fde0cac
Hariharan-081206 <harivisu06@gmail.com>
2025-06-05 12:13:15 +0530

    Database Schema for events

---
+++ models/eventSchema.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+const eventSchema = new mongoose.Schema({
+  title: {type: String,required: true},
+  description:{type: String},
+  collegeName:{type: String},
+  venue:{
+    roomNumber:String,
+    location: String,
+    capacity: Number,
+  },
+  EventDate:{type:Date},
+  startTime:{type:String},
+  endTime:{type: String},
+  isRegistrationOpen:{type: Boolean,default: true},
+
+}, { timestamps: true });
+/*
+ export default mongoose.model('event',eventSchema);*/
+
+const Event = mongoose.model('Event', eventSchema);
+export default Event;
