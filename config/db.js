c71da78d9783485eb43e1010802d61c57bcfb836
Hariharan-081206 <harivisu06@gmail.com>
2025-06-11 08:16:51 +0530

    project database

--- config/db.js
+++ config/db.js
@@ -1,4 +1,35 @@
-import mongoose from "mongoose"
+// // ./config/db.js
+// import mongoose from 'mongoose';
+
+// export async function connectProjectDB() {
+//   try {
+//     const projectDb = await mongoose.createConnection(process.env.MONGO_URI, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`✅ Connected to Project DB,${projectDb.name}`);
+//     return projectDb;
+//   } catch (error) {
+//     console.error('Failed to connect Project DB', error);
+//     process.exit(1);
+//   }
+// }
+
+// export async function connectVenueDB() {
+//   try {
+//     const venueDb = await mongoose.createConnection(process.env.MONGO_URI2, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     console.log(`✅ Connected to Venue DB,${venueDb.client.s.url}`);
+//     return venueDb;
+//   } catch (error) {
+//     console.error('Failed to connect Venue DB', error);
+//     process.exit(1);
+//   }
+// }
+
+import mongoose from "mongoose";
 
 export const connectDB = async()=>{
     try{
@@ -10,3 +41,16 @@ export const connectDB = async()=>{
     } 
 }
 
+export async function connectVenueDB() {
+  try {
+    const venueDb = await mongoose.createConnection(process.env.MONGO_URI2, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`✅ Connected to Venue DB: ${venueDb.client.s.url}`);
+    return venueDb;
+  } catch (error) {
+    console.error("❌ Failed to connect Venue DB", error);
+    process.exit(1);
+  }
+}
