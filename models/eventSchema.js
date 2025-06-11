3206ed094393f5206246e9bdd6870e7813df8d1d
Hariharan-081206 <harivisu06@gmail.com>
2025-06-11 08:18:05 +0530

    database schema for event

--- models/eventSchema.js
+++ models/eventSchema.js
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+/*import mongoose from 'mongoose';
 const eventSchema = new mongoose.Schema({
   title: {type: String,required: true},
   description:{type: String},
@@ -14,8 +14,53 @@ const eventSchema = new mongoose.Schema({
   isRegistrationOpen:{type: Boolean,default: true},
 
 }, { timestamps: true });
+const createEventModel=mongoose.model('Event',eventSchema);
 /*
- export default mongoose.model('event',eventSchema);*/
+ export default mongoose.model('event',eventSchema);
 
 const Event = mongoose.model('Event', eventSchema);
-export default Event;
+export default createEventModel;*/
+
+/*import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String },
+  collegeName: { type: String },
+  venue: {
+    roomNumber: String,
+    location: String,
+    capacity: Number,
+  },
+  EventDate: { type: Date },
+  startTime: { type: String },
+  endTime: { type: String },
+  isRegistrationOpen: { type: Boolean, default: true },
+}, { timestamps: true });
+
+export default function createEventModel(connection) {
+  return connection.model('Event', eventSchema);
+}
+*/
+
+import mongoose from 'mongoose';
+
+const eventSchema = new mongoose.Schema({
+  title: String,
+  description: String,
+  collegeName: String,
+  venueDetails: {
+    venueId: String,
+    roomnumber: String,
+    capacity: Number,
+    location: String,
+  },
+  startTime: Date,
+  endTime: Date,
+  EventDate: Date
+});
+
+export default function createEventModel(db) {
+  return db.models.Event || db.model('Event', eventSchema);
+}
+
